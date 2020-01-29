67763f4f3f8282810c12daaaf8cc3bff80291423
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-01-29 17:38:30 -0500

    Add files via upload

--- CSCI340_P1.c
+++ CSCI340_P1.c
@@ -1,6 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+
 struct process{
     int pid;
     int ppid;
     int vsize;
+    struct process *parent;
     char name[];
 };
+
+int main(int argv,char *argc[]){
+    DIR *directory;
+    struct dirent *entry;
+    int count;
+}
