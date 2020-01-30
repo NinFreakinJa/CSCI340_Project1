b08c58fde5b63bf2cdbca86d4d3df2016deac370
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-01-29 19:57:06 -0500

    Add files via upload

--- CSCI340_P1.c
+++ CSCI340_P1.c
@@ -28,14 +28,18 @@ int main(int argv,char *argc[]){
         return 0;
     }
     int size=300;
+    //linked list of processes
+    //probably need a better type of list for later making the tree
     struct process *pList=malloc(sizeof(struct process)*300);
     while((entry=readdir(drproc))!=NULL){
         char *directoryName=entry->d_name;
+        //checks if directory name is a number
         if(*directoryName<'0'||*directoryName>'9'){
             continue;
         }
         else{
             count++;
+            //increases list size if it gets too big
             if(count>=size){
                 pList=realloc(pList,sizeof(struct process)*(size+50));
                 size+=50;
@@ -43,9 +47,15 @@ int main(int argv,char *argc[]){
             //just checking if working
             //REMOVE
             printf("%s\n",directoryName);
+
+            //Parse through /proc/<pid>/stat file and get relevent info
+            //put info into struct process and into pList
         }
     }
+    //create tree
+
     //REMOVE
     printf("%d",count);
+    
     closedir(drproc);
 }
